4ef89f76e1f7c2810c50f580711064e32dde07c8
Tom Gillooly <tomgillooly@gmail.com>
2017-11-04 23:39:21 +0100

    Revert OpenGL version, set log level verbose

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 
 //========================================================================
 int main( ){
-
-	// ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
-    ofGLWindowSettings settings;
-    settings.setGLVersion(3,2);
-    ofCreateWindow(settings);
+    ofSetLogLevel(OF_LOG_VERBOSE);
+	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
+    // ofGLWindowSettings settings;
+    // settings.setGLVersion(3,2);
+    // ofCreateWindow(settings);
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
